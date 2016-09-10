4946a5d247c1cf4b3dc5be180aceaef59b9a75b9
Isaac Paul <izackp@gmail.com>
2016-09-09 21:29:23 -0400

    Added EventDrivenTCPClient, WeakAction, and some more stuff

--- Extensions/ArrayExt.cs
+++ Extensions/ArrayExt.cs
@@ -1,12 +1,42 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System;
 
 public static class ArrayExt {
 
 	public static T SafeObjectAt<T>(this IList<T> list, int index) {
-		if (index >= list.Count) 
+		if (index >= list.Count || index < 0) 
 			return default(T);
 
 		return list[index];
 	}
+    /*
+    public static T Last<T>(this IList<T> list) {
+        int index = list.Count - 1;
+        if (index < 0)
+            return default(T);
+
+        return list[index];
+    }
+
+    public static T First<T>(this IList<T> list) {
+        if (list.Count == 0)
+            return default(T);
+
+        return list[0];
+    }*/
+
+    public static T[] SubArray<T>(this T[] data, int index, int length) {
+        T[] result = new T[length];
+        Array.Copy(data, index, result, 0, length);
+        return result;
+    }
+
+    public static T[] Concat<T>(this T[] x, T[] y) {
+        if (x == null) throw new ArgumentNullException("x");
+        if (y == null) throw new ArgumentNullException("y");
+        int oldLen = x.Length;
+        Array.Resize(ref x, x.Length + y.Length);
+        Array.Copy(y, 0, x, oldLen, y.Length);
+        return x;
+    }
 }
---
+++ Utility/WeakAction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Reflection;
+
+public class WeakAction {
+    WeakReference weakHolder;
+    MethodInfo method;
+
+    public WeakAction(Action action) {
+        weakHolder = new WeakReference(action.Target);
+        method = action.Method;
+    }
+
+    public void Execute() {
+        object strongTarget = null;
+
+        if (weakHolder.IsAlive)
+            strongTarget = weakHolder.Target;
+        method.Invoke(strongTarget, new object[] { });
+    }
+
+#if UNITY_5_3_OR_NEWER
+    public static implicit operator UnityEngine.Events.UnityAction(WeakAction m) {
+
+        return m.Execute;
+    }
+#endif
+
+    public static implicit operator Action(WeakAction m) {
+
+        return m.Execute;
+    }
+}
