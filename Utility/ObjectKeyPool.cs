d2f2fe5475f7543f8b8fb6ab7bd5ef83b373014c
Isaac Paul <izackp@gmail.com>
2017-06-20 01:12:20 -0400

    Added ObjectKeyPool. Minor Revision to WeakAction.

---
+++ Utility/ObjectKeyPool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using CSharp_Library.Extensions;
+
+namespace CSharp_Library.Utility {
+
+    public class ObjectKeyPool {
+
+        Dictionary<string, List<object>> _poolData = new Dictionary<string, List<object>>();
+
+        public void Add(string key, object obj) {
+            if (key == null)
+                return;
+
+            List<object> listForKey = _poolData.GetValueSafe(key);
+            if (listForKey == null) {
+                listForKey = new List<object>();
+                _poolData[key] = listForKey;
+            }
+            listForKey.Add(obj);
+            //Debug.LogWarning("+" + key + "(" + listForKey.Count + ")");
+        }
+
+        public object Retrieve(string key) {
+            if (key == null)
+                return null;
+
+            List<object> listForKey = _poolData.GetValueSafe(key);
+            if (listForKey == null)
+                return null;
+
+            if (listForKey.Count == 0)
+                return null;
+
+            object obj = listForKey[listForKey.Count - 1];
+            listForKey.RemoveAt(listForKey.Count - 1);
+            //Debug.LogWarning("-" + key + "(" + listForKey.Count + ")");
+            return obj;
+        }
+    }
+}
