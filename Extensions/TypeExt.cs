6cd6db08477287d47a235039d2cf86ccce6c2070
Isaac Paul <izackp@gmail.com>
2017-08-26 13:29:48 -0400

    Added Type Extensions

---
+++ Extensions/TypeExt.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CSharp_Library.Extensions {
+    public static class TypeExt {
+        // In 4.5+, TypeInfo has most of the reflection methods previously on type
+        // This allows code to be shared between 3.5 && 4.5+ projects
+        public static Type GetTypeInfo(this Type type) {
+            return type;
+        }
+    }
+}
