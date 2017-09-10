b47bfac3477c8c05b04d7741e3ba082b22b31415
Isaac Paul <izackp@gmail.com>
2017-09-10 14:45:04 -0400

    Added Comparison Enum

---
+++ Utility/ComparisonEnum.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CSharp_Library.Utility {
+
+    /// <summary>
+    /// Gives an appropiate name for the number to return from CompareTo()
+    /// </summary>
+    enum Comparison {
+        Before = -1,
+        Same = 0,
+        After = 1
+    }
+}
